ce7516297014fbb83913f432bf6e742485d7fd3b
Lewis-Allen-2001 <itslewbie01@gmail.com>
2024-09-16 15:59:21 +0100

    using props in react

--- week6/react/reac-props/props/src/components/Gallery.jsx
+++ week6/react/reac-props/props/src/components/Gallery.jsx
@@ -3,11 +3,36 @@ import Painting from "./Painting";
 export default function Gallery() {
   return (
     <div>
+      <h2>Shiver</h2>
       <Painting img_url="https://the-comics-journal.sfo3.digitaloceanspaces.com/wp-content/uploads/2017/12/ItoCover0001-e1513567192492-768x618.jpg" />
+      <p>
+        This volume includes nine of Junji Ito’s best short stories, as selected
+        by the author himself and presented with accompanying notes and
+        commentary. An arm peppered with tiny holes dangles from a sick girl’s
+        window… After an idol hangs herself, balloons bearing the faces of their
+        destined victims appear in the sky… An amateur film crew hires an
+        extremely individualistic fashion model and faces a real bloody ending…
+        An offering of nine fresh nightmares for the delectation of horror fans.
+      </p>
 
+      <h2>Tomie</h2>
       <Painting img_url="https://i0.wp.com/wherecreativityworks.com/wp-content/uploads/2023/07/tomie_1.jpeg?resize=540%2C528&ssl=1" />
+      <p>
+        Tomie Kawakami is a femme fatale with long black hair and a beauty mark
+        just under her left eye. She can seduce nearly any man, and drive them
+        to murder as well, even though the victim is often Tomie herself. While
+        one lover seeks to keep her for himself, another grows terrified of the
+        immortal succubus. But soon they realize that no matter how many times
+        they kill her, the world will never be free of Tomie.
+      </p>
 
+      <h2>The Hanging Balloons</h2>
       <Painting img_url="https://media.mutualart.com/Images/2017_10/23/09/090040001/55fb9eb8-8c09-4bd4-8224-9dd854dac13c_570.Jpeg" />
+      <p>
+        The Hanging Balloons is the sixth chapter from volume 4 of the Horror
+        World of Junji Ito Collection, The Face Burglar. The story received a
+        sequel entitled Return of the Hanging Balloons.
+      </p>
     </div>
   );
 }
